205bc55ebd9644e151e421dd89a75e9b81682290
Jverg <giannisverg@gmail.com>
2017-06-10 07:49:33 -0700

    Add a favicon

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -13,6 +13,9 @@
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+{{-- Favicon --}}
+<link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}">
+
 {{ Html::style('css/styles.css') }}
 
 @yield('stylesheets')
