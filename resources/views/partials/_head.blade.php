f1e3de1f37733743cf47f698f90a90aa18c314c5
Jverg <giannisverg@gmail.com>
2017-08-15 11:11:51 -0700

    Small changes in comments

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,11 +1,12 @@
 
-<!-- Head file -->
+<!-- Header file -->
 
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<title>Grouplend @yield('title')</title> <!-- CHANGE THIS TITLE FOR ITS PAGE -->
+<title>Grouplend @yield('title')</title>
 
 {{-- Comforta text. --}}
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Comfortaa" />
